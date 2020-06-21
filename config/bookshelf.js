62cda964e67d5ba1c255ec7c477f74e17cadef71
Yih Yang <yyngo788n88@hotmail.com>
2020-06-21 13:03:05 +0800

    added env settings

--- config/bookshelf.js
+++ config/bookshelf.js
@@ -1,4 +1,5 @@
-const knexConfig = require('./../knexfile').development;
+const env = process.env.NODE_ENV || 'development';
+const knexConfig = require('./../knexfile')[env];
 const Knex = require('knex');
 const Bookshelf = require('bookshelf');
 
