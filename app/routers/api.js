2bdcd68ebc2a2f6f8565a8ca29f821668441acf1
Yih Yang <yyngo788n88@hotmail.com>
2020-06-21 12:11:20 +0800

    added ping route

--- app/routers/api.js
+++ app/routers/api.js
@@ -1,10 +1,23 @@
 const rootPath = './../..'
 const api = require(`${rootPath}/app/controllers/api`)
 const passport = require('passport')
+const {respondSuccess} = require(`${rootPath}/app/helpers/response`)
 
 let skipAuthticationPath = [];
 
 module.exports = (app) => {
+  app.get('/_ping', function(req, res) {
+    res.status(200).json(
+      respondSuccess(
+        'Success',
+        'Successfully pinged the server'
+      )
+    );
+  });
+
+  app.post('/auth', api.auth.authenticate)
+  app.post('/auth/employee-id', api.auth.authenticateWithEmployeeId)
+
   // app the path that starts with /api/v1 will be authenticated
   app.use('/v1', function(req, res, next) {
     if (!skipAuthticationPath.includes(req.url)) {
@@ -15,9 +28,6 @@ module.exports = (app) => {
       next();
     }
   });
-
-  app.post('/auth', api.auth.authenticate)
-  app.post('/auth/employee-id', api.auth.authenticateWithEmployeeId)
 
   app.get('/v1/nodes/name/:name', api.v1.node.showByName)
   app.post('/v1/po-records', api.v1.poRecord.save)
